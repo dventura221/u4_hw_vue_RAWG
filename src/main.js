f731614eddc88a10bf9cf4cf2416ecb010e0dbca
Dan Ventura <danice.ventura@gmail.com>
2022-04-28 09:42:49 -0600

    added routes, installed vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router.js'
 import './styles/index.css'
 import './styles/App.css'
 
